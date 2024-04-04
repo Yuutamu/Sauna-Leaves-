cde9572b6ca058fb013a8113ea772c1e9c7b6eea
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-04-04 22:11:47 +0900

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :customers
+  devise_for :admins
+  root to: 'pages#home'
 
-  get "/up/", to: "up#index", as: :up
-  get "/up/databases", to: "up#databases", as: :up_databases
+  get '/up/', to: 'up#index', as: :up
+  get '/up/databases', to: 'up#databases', as: :up_databases
 
   # Sidekiq has a web dashboard which you can enable below. It's turned off by
   # default because you very likely wouldn't want this to be available to
