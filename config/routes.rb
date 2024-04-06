93743517bf874c66ce44d52f3c7236aa95887ba4
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-04-06 21:50:09 +0900

    Admin-注文履歴の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
     sessions: 'admin/sessions'
@@ -7,9 +9,16 @@ Rails.application.routes.draw do
     registrations: 'customer/registrations'
   }
   root to: 'pages#home'
-
   namespace :admin do
+    root to: 'pages#home'
     resources :products, only: %i[index show new create edit update]
+    resources :orders, only: %i[show update]
+    resources :customers, only: %i[index show update]
+
+    # sidekiq 管理画面
+    authenticate :admin do
+      mount Sidekiq::Web => '/sidekiq'
+    end
   end
 
   scope module: :customer do
@@ -21,14 +30,31 @@ Rails.application.routes.draw do
         patch 'decrease'
       end
     end
-    resources :checkouts, only: [:create]
-    resources :webhooks, only: [:create]
-    resources :orders, only: %i[index,show] do
+
+    # Stripe
+    resources :checkouts, only: [:create] # Stripe | createアクションのみ
+    resources :webhooks, only: [:create] # Stripe | createアクションのみ
+
+    # success
+    resources :orders, only: %i[index show] do
       collection do
         get 'success'
       end
     end
+
+    # 退会
+    resources :customers do
+      collection do
+        get 'confirm_withdraw'
+        patch 'withdraw'
+      end
+    end
   end
+
+  # 静的なページ
+  get  'law_pages/privacy_policy'
+  get  'law_pages/tokusyo'
+  get  'law_pages/terms'
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
