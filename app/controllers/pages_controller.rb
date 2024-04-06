98652ce18eb249114f1a13815a23005a11e829eb
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-04-06 23:25:47 +0900

    toppage の商品掲載ロジックの実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,14 @@
+# TopPage 商品表示処理
+
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @products = products_in_descending
+  end
+
+  private
+
+  # MEMO:UI観点から、おすすめ商品を価格の高い３商品を表示
+  def products_in_descending
+    Product.price_high_to_low.limit(3)
+  end
 end
