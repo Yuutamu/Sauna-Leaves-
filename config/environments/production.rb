154d16bfd89e129a0d9afef3010ade3dd64d45a6
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-04-10 12:33:08 +0900

    assets pipeline の設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # MEMO:本番環境エラー対応のために false→true に変更
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
