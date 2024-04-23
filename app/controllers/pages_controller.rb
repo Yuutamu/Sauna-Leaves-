117fc1c05eef71c5c279ebec70bf817fa4dd21e5
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-04-23 09:11:43 +0900

    おすすめ商品の掲載ロジック

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,13 @@ class PagesController < ApplicationController
 
   private
 
-  # MEMO:UI観点から、おすすめ商品を価格の高い３商品を表示
-  def products_in_descending
-    Product.price_high_to_low.limit(3)
+  # # MEMO:UI観点から、おすすめ商品を価格の高い３商品を表示
+  # def products_in_descending
+  #   Product.price_high_to_low.limit(3)
+  # end
+
+  # MEMO:おすすめ商品を価格の低い10商品を表示
+  def products_in_ascending
+    Product.price_low_to_high.limit(10)
   end
 end
