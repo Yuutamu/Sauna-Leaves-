06e908f806b70ef6980b6c4a2f4f9c98a3c4ec3b
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-05-14 19:53:09 +0900

    画面遷移図等の説明をreadmeに追加

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -5,7 +5,7 @@ class Customer::ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @cart_item = CartItem.new # MEMO:商品詳細ページにカート追加ボタンを設置
+    @cart_item = CartItem.new
   end
 
   private
@@ -13,10 +13,13 @@ class Customer::ProductsController < ApplicationController
   def get_products(params)
     return Product.all, 'default' unless params[:latest] || params[:price_high_to_low] || params[:price_low_to_high]
 
+    # メモ：Product.where(order(created_at: :desc))
     return Product.latest, 'latest' if params[:latest]
 
-    return Product.price.price_high_to_low, 'price_high_to_low' if params[:price_high_to_low]
+    # メモ：Product.where(order(price: :desc))
+    return Product.price_high_to_low, 'price_high_to_low' if params[:price_high_to_low]
 
-    [Product.price.price_low_to_high, 'price_low_to_high'] if params[:price_low_to_high]
+    # メモ：Product.where(order(price: :asc))
+    [Product.price_low_to_high, 'price_low_to_high'] if params[:price_low_to_high]
   end
 end
